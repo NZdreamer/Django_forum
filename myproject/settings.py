a59014e896003be5c6901ebd34a222dfe48e2d1c
lucifer <hi45288871@gmail.com>
2023-02-18 05:22:27 +0000

    change key location

--- myproject/settings.py
+++ myproject/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$r1tmie=a)^)op$l=8%$xq6g#h&(h6%3=tfhe*gz-j9mqb8ax0'
+SECRET_KEY = os.environ['SECRET_KEY']
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
